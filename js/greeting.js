2103271ceab14b1ed8d0e109ad1e9c5cd4fd15e1
Sun970324 <ysw5202222@gmail.com>
2022-01-26 00:49:02 +0900

    Test

--- js/greeting.js
+++ js/greeting.js
@@ -17,7 +17,7 @@ function onLoginSubmit(event) {
   localStorage.setItem(USERNAME_KEY, username);
   paintGreeting(username);
 }
-
+console.log("testing")
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
