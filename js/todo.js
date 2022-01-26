f106e27f46061e06479e19fa2ba2550d6a13641f
Sun970324 <ysw5202222@gmail.com>
2022-01-26 23:31:04 +0900

    todo 수정

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,15 @@
 const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todo-list");
+const createList = document.createElement("li")
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value
   toDoInput.value=""
+  console.log(newToDo);
+  createList.textContent = newToDo
+  toDoList.appendChild(createList)
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
